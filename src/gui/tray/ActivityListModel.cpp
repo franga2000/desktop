cdfd2f44400a2367f6e7ca423c281c50e24c9d23
alex-z <blackslayer4@gmail.com>
2021-10-02 00:08:32 +0300

    Refactoring. Removed redundant changes.
    Signed-off-by: alex-z <blackslayer4@gmail.com>

--- src/gui/tray/ActivityListModel.cpp
+++ src/gui/tray/ActivityListModel.cpp
@@ -24,6 +24,7 @@
 #include "accountmanager.h"
 #include "conflictdialog.h"
 #include "folderman.h"
+#include "iconjob.h"
 #include "accessmanager.h"
 #include "owncloudgui.h"
 #include "guiutility.h"
